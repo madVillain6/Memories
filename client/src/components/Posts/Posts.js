66c3f9c05046e1a5f0c50ed2b916327c2c959702
madVillain6 <levanbichinashvili5@gmai.com>
2023-03-26 22:53:45 +0400

    feat:created schema and api

---
+++ client/src/components/Posts/Posts.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Post from "./post/post";
+import { useSelector } from "react-redux";
+
+const Posts = () => {
+  const posts = useSelector((state) => state.posts);
+  console.log(posts);
+  return (
+    <>
+      <h1>Posts</h1>
+      <Post />
+      <Post />
+      <Post />
+    </>
+  );
+};
+
+export default Posts;
